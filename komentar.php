9e6f81653a916822cd90a1455bbb24ffb7476d3c
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2018-10-01 10:17:05 +0700

    Progress Komentar

--- komentar.php
+++ komentar.php
@@ -1,7 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Komentar</title>
+	</head>
+	<body>
+		<form action=" " method="post">
+			<table>
+				<tr>
+					<td>Nama</td>
+					<td><input type="text" name="nama" required></td>
+				</tr>
+				<tr>
+					<td>Komentar</td>
+					<td><textarea name="komentar"></textarea></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" name="submit" value="Submit"></td>
+				</tr>
+			</table>
+		</form>
+	</body>
+</html>
+
 <?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
-
-
-
-
+if (isset($_POST['submit'])) {
+	$nama 		= isset($_POST['nama']) ? $_POST['nama'] : "";
+	$komentar 	= isset($_POST['komentar']) ? $_POST['komentar'] : "";
+	$data = explode(" ", $komentar);
+	if (count($data) < 5) {
+		echo "Komentar Anda Harus Lebih Dari 5 Kata!";
+	} else {
+		echo "Komentar anda : <br>" . $komentar;
+	}
+}
 ?>
